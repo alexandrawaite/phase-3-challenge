111f1562a2ca958ebb448ec7d63c25dee2f1d1e8
alexandrawaite <alexandrawaite@gmail.com>
2017-09-18 13:41:18 -0700

    initial commit

---
+++ part-1/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const daysOfWeek = {
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 7
+}
+
+app.get('/api/days/:day', (req, res) => {
+  const day = req.params.day;
+
+  if (!daysOfWeek[day]) {
+    res.status(400).send(`'${day}' is not a valid day!`);
+  }
+  res.send(`${daysOfWeek[day]}`);
+});
+
+app.post('/api/array/concat', (req, res) => {
+  const array1 = req.body.array1;
+  const array2 = req.body.array2;
+  const mergedArray = array1.concat(array2);
+
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    res.status(400).send({"error": "Input data should be of type Array."});
+  } else {
+  res.send({"result": `${mergedArray}`});
+  }
+});
+
+app.listen(3000, () => {
+  console.log('The app is running on localhost:3000');
+});
