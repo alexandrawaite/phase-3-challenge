823e95be9f38bc6eb13e714324f03f09a52a3486
alexandrawaite <alexandrawaite@gmail.com>
2017-09-19 15:26:03 -0700

    complete store script with commands

---
+++ part-2/store.js
@@ -0,0 +1,32 @@
+#!/usr/local/bin/node
+const queries = require('./database');
+const print = require('node-print');
+
+const command = process.argv[2];
+const argument = process.argv[3];
+
+switch (command) {
+  case 'product-list':
+    queries.listProductSection(argument)
+    .then((results) => {
+      print.pt(results);
+      process.exit();
+    });
+    break;
+  case 'shopper-orders':
+    queries.listShopperOrder(argument)
+    .then((results) => {
+      print.pt(results);
+      process.exit();
+    });
+    break;
+  case 'real-shoppers':
+    queries.listShoppers()
+    .then((results) => {
+      print.pt(results);
+      process.exit();
+    });
+    break;
+  default:
+  console.log('Please enter a valid command');
+}
