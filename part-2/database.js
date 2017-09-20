6f52c17834b70bd50303ebe5c741cca36d232945
alexandrawaite <alexandrawaite@gmail.com>
2017-09-20 10:19:22 -0700

    final edit

--- part-2/database.js
+++ part-2/database.js
@@ -23,9 +23,9 @@ const queries = {
       SELECT orders.id as order_id, SUM(CAST(grocery_items.price AS REAL)) AS total_cost
       FROM orders
         INNER JOIN items_orders
-        ON items_orders.order_id = orders.id
+          ON items_orders.order_id = orders.id
         INNER JOIN grocery_items
-        ON grocery_items.id = items_orders.item_id
+          ON grocery_items.id = items_orders.item_id
       WHERE shopper_id = $1
       GROUP BY orders.id
     `, [shopper]);
@@ -33,10 +33,10 @@ const queries = {
 
   listShoppers: () => {
     return db.any(`
-      SELECT shoppers.name AS shopper_name, COUNT(CAST(orders.id AS INTEGER)) AS total_orders
+      SELECT shoppers.name AS shopper_name, CAST(COUNT(orders.id) AS INTEGER) AS total_orders
       FROM shoppers
         INNER JOIN orders
-        ON shoppers.id = orders.shopper_id
+          ON shoppers.id = orders.shopper_id
       WHERE orders.id >= 1
       GROUP BY shoppers.id
     `);
