63f9712caa6ef67577e1311633666316b932b7b9
alexandrawaite <alexandrawaite@gmail.com>
2017-09-20 09:19:10 -0700

    refactor query to list shoppers' orders

--- part-2/database.js
+++ part-2/database.js
@@ -1,4 +1,3 @@
-// All database query functions are written in a file database.js
 const pgp = require('pg-promise')();
 
 const cn = {
@@ -10,27 +9,37 @@ const cn = {
 const db = pgp(cn);
 
 const queries = {
+
   listProductSection: (section) => {
-    return db.any(`SELECT * FROM grocery_items WHERE section = $1`, [section]);
+    return db.any(`
+      SELECT name AS product_name, section
+      FROM grocery_items
+      WHERE section = $1
+      `, [section]);
   },
-  listShopperOrder: (shopper) => {
-    return db.any(`SELECT orders.id as order_id, SUM(CAST(grocery_items.price AS DECIMAL)) AS total_cost
-    FROM orders
-    INNER JOIN items_orders
-    ON items_orders.order_id = orders.id
-    INNER JOIN grocery_items
-    ON grocery_items.id = items_orders.item_id
-    WHERE shopper_id = $1
-    GROUP BY orders.id
+
+  listShopperOrders: (shopper) => {
+    return db.any(`
+      SELECT orders.id as order_id, SUM(CAST(grocery_items.price AS REAL)) AS total_cost
+      FROM orders
+        INNER JOIN items_orders
+        ON items_orders.order_id = orders.id
+        INNER JOIN grocery_items
+        ON grocery_items.id = items_orders.item_id
+      WHERE shopper_id = $1
+      GROUP BY orders.id
     `, [shopper]);
   },
+
   listShoppers: () => {
-    return db.any(`SELECT shoppers.name AS shopper_name, COUNT(CAST(orders.id AS INTEGER)) AS total_orders
-    FROM shoppers
-    INNER JOIN orders
-    ON shoppers.id = orders.shopper_id
-    WHERE orders.id >= 1
-    GROUP BY shoppers.id`);
+    return db.any(`
+      SELECT shoppers.name AS shopper_name, COUNT(CAST(orders.id AS INTEGER)) AS total_orders
+      FROM shoppers
+        INNER JOIN orders
+        ON shoppers.id = orders.shopper_id
+      WHERE orders.id >= 1
+      GROUP BY shoppers.id
+    `);
   }
 }
 
